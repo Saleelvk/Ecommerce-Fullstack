e42fda15cd6a8b5c8917093bade3aeaaa26b5812
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-29 09:25:39 +0530

    Update product-rout.js

--- backend/routes/product-rout.js
+++ backend/routes/product-rout.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const upload = require('../utils/image-upload'); // Your multer config
 const { getProducts, addProduct, editProduct, deleteProduct, getProductsById } = require('../controllers/product-controller');
-const { adminOnly,authMiddleware } = require('../middlewares/auth-middleware');
+const { adminOnly, authMiddleware } = require('../middlewares/auth-middleware');
 const router = express.Router();
 
-router.use('/uploads',express.static('uploads'));
-router.get('/products', getProducts); 
+// Routes for products
+router.get('/products', getProducts);
 router.get('/products/:id', getProductsById);
 
-//admin routes
-router.post('/addProduct',authMiddleware,adminOnly,upload.array('images', 5), addProduct);
-router.put('/editproduct/:id',authMiddleware,adminOnly,upload.array('images', 5), editProduct);
-router.delete('/deleteProduct/:id',authMiddleware,adminOnly , deleteProduct);
-
+// Admin routes with authentication and authorization
+router.post('/addProduct', authMiddleware, adminOnly, upload.array('images', 5), addProduct);
+router.put('/editproduct/:id', authMiddleware, adminOnly, upload.array('images', 5), editProduct);
+router.delete('/deleteProduct/:id', authMiddleware, adminOnly, deleteProduct);
 
 module.exports = router;
- 

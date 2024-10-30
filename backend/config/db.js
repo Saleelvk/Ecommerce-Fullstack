a1a03bcd20bfd529a3062cd5298fdba0bdc55c2d
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-30 09:13:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-    await mongoose.connect(process.env.MONGODB_URL)
+    await mongoose.connect(process.env.Mongo_url)
         .then(() => {
             console.log("database connected");
         })

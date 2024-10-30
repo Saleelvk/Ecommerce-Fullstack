bdec45a137608cd0723c3a7cae87ca651a61db1a
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-10-30 11:02:11 +0530

    Update ShopContext.jsx

--- frontend/src/components/ShopContext.jsx
+++ frontend/src/components/ShopContext.jsx
@@ -5,7 +5,7 @@ export const ShopContext = createContext();
 const ShopProvider = ({ children }) => {
     const [cartItems, setCartItems] = useState([]);
     const currency = '$'; // Set your desired currency
-    const url="https://project-b-q3cz.onrender.com" 
+    const url="https://e-commerce-gclo.onrender.com" 
   
     // Helper function to update cart in the backend
     const updateCartInBackend = async (productId, quantity) => { 

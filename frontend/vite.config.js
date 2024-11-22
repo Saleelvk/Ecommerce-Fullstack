bcbf06c5e4af280a37c448afbd503fb65daac39c
Saleelvk <87797952+Saleelvk@users.noreply.github.com>
2024-11-22 15:15:32 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // No proxy configuration
+  build: {
+    outDir: 'build',  // Set the output directory to 'build'
+  },
 });
-  

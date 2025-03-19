ce4b732e56050e5f8d39ec17e613640f615f2beb
Saleel vk <87797952+Saleelvk@users.noreply.github.com>
2025-03-19 15:23:58 +0530

    Update generate-token.js

--- backend/utils/generate-token.js
+++ backend/utils/generate-token.js
@@ -10,7 +10,7 @@ const generateToken = (res, userId) => {
         httpOnly: true, 
         secure: process.env.NODE_ENV === 'production',
         sameSite: 'None',
-        maxAge: 24 * 60 * 60 * 1000,
+        maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
     return token; // Return the token

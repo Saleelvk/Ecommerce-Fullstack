ebf6d0dc9daccf38cd1385334ca342e905c95c66
Saleel vk <87797952+Saleelvk@users.noreply.github.com>
2025-03-19 15:22:49 +0530

    Update generate-token.js

--- backend/utils/generate-token.js
+++ backend/utils/generate-token.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 const generateToken = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: '1d',
+        expiresIn: '30d',
     });
     
     // Set the JWT as an HTTP-only cookie
